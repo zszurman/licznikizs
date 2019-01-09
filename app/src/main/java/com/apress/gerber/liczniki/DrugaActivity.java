b362db9f524e77aaba7558240c71d75d001b8a91
Zbigniew Szurman <zszurman@wp.pl>
2019-01-09 23:34:55 +0100

    wersja Intent

---
+++ app/src/main/java/com/apress/gerber/liczniki/DrugaActivity.java
@@ -0,0 +1,67 @@
+package com.apress.gerber.liczniki;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class DrugaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_druga);
+
+        Intent dupa = getIntent();
+        TextView ts1 = findViewById(R.id.ts1);
+        TextView ts2 = findViewById(R.id.ts2);
+        TextView ts3 = findViewById(R.id.ts3);
+
+        TextView bts1 = findViewById(R.id.bts1);
+        TextView bts2 = findViewById(R.id.bts2);
+        TextView bts3 = findViewById(R.id.bts3);
+
+        ts1.setText(dupa.getStringExtra("aupa"));
+        ts2.setText(dupa.getStringExtra("bupa"));
+        ts3.setText(dupa.getStringExtra("cupa"));
+
+        bts1.setText(dupa.getStringExtra("xupa"));
+        bts2.setText(dupa.getStringExtra("yupa"));
+        bts3.setText(dupa.getStringExtra("zupa"));
+    }
+
+    public void dzwon(View view) {
+
+        Intent tel = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+48324552634"));
+        if (tel.resolveActivity(getPackageManager()) != null) {
+            startActivity(tel);
+        }
+    }
+
+    public void wyslij(View view) {
+
+        Intent stronka = new Intent(Intent.ACTION_VIEW, Uri.parse("https://ebok.tauron.pl/"));
+        if (stronka.resolveActivity(getPackageManager()) != null) {
+            startActivity(stronka);
+        }
+    }
+
+    public void rob(View view) {
+        String[] jola= {"jolszurman@wp.pl"};
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/html");
+        intent.putExtra(Intent.EXTRA_EMAIL, jola);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Od Zyszka");
+        intent.putExtra(Intent.EXTRA_TEXT, "Cześć, testuja program. Miłych snów:)");
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+
+
+        startActivity(Intent.createChooser(intent, "Send Email"));
+
+
+    }
+    }
+}
--- app/src/main/java/com/apress/gerber/liczniki/MainActivity.java
+++ app/src/main/java/com/apress/gerber/liczniki/MainActivity.java
@@ -1,17 +1,20 @@
 package com.apress.gerber.liczniki;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private int stanLW =1;
-    private int bazaLW = 408;
+    private int stanLW = 1;
+    private int bazaLW = 410;
     private double abW = 12.20; // woda opłaty stałe
-    private int doW = 5;
+    private int doW = 8;
 
     private int stanL94 = 1;
     private int bazaL94 = 30932;
@@ -24,17 +27,22 @@ public class MainActivity extends AppCompatActivity {
     private double ab95 = (7 + 6.50 + 0.38 + 6.49 + 6) * 1.23; // L95 opłaty stałe
     private int do95 = 5;
     private String n95 = "L95";
-                                          // ceny jednostkowe podać w abstract class Suma
-    public MainActivity(){
+    // ceny jednostkowe podać w abstract class Suma
+
+
+    public MainActivity() {
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
-        TextView textW= findViewById(R.id.editText3);
-        TextView textL1= findViewById(R.id.editText4);
-        TextView textL2= findViewById(R.id.editText5);
+        TextView textW = findViewById(R.id.editText3);
+        TextView textL1 = findViewById(R.id.editText4);
+        TextView textL2 = findViewById(R.id.editText5);
+        Button druga = findViewById(R.id.button4);
         textW.setText("Woda odczyt: " + String.valueOf(bazaLW));
         textL1.setText("L94 odczyt: " + String.valueOf(bazaL94));
         textL2.setText("L95 odczyt: " + String.valueOf(bazaL95));
@@ -43,36 +51,71 @@ public class MainActivity extends AppCompatActivity {
     public void zmW(View view) {
         TextView textW = findViewById(R.id.editText3);
         TextView podW = findViewById(R.id.editText6);
-        if   ((podW.getText().length() != 0))
-             stanLW = Integer.parseInt(podW.getText().toString());
+        if ((podW.getText().length() != 0))
+            stanLW = Integer.parseInt(podW.getText().toString());
         else stanLW = 1;
-        if   ((stanLW < (bazaLW +300)) && (stanLW >= bazaLW)) {
+        if ((stanLW < (bazaLW + 300)) && (stanLW >= bazaLW)) {
             Suma woda = new Woda(stanLW, bazaLW, abW, doW);
             textW.setText(woda.toString());
-       }else textW.setText("nieprawidłowe dane");
+        } else textW.setText("nieprawidłowe dane");
     }
 
     public void zmL1(View view) {
         TextView textL1 = findViewById(R.id.editText4);
         TextView podL1 = findViewById(R.id.editText7);
-        if   ((podL1.getText().length() != 0))
-             stanL94 = Integer.parseInt(podL1.getText().toString());
+        if ((podL1.getText().length() != 0))
+            stanL94 = Integer.parseInt(podL1.getText().toString());
         else stanL94 = 1;
-        if   ((stanL94 >= bazaL94 ) && (stanL94 < (bazaL94 + 2000))) {
-              Suma L94 = new Prad(n94, stanL94, bazaL94, ab94, do94);
-              textL1.setText(L94.toString());
-      } else  textL1.setText("nieprawidłowe dane");
+        if ((stanL94 >= bazaL94) && (stanL94 < (bazaL94 + 2000))) {
+            Suma L94 = new Prad(n94, stanL94, bazaL94, ab94, do94);
+            textL1.setText(L94.toString());
+        } else textL1.setText("nieprawidłowe dane");
     }
 
     public void zmL2(View view) {
         TextView textL2 = findViewById(R.id.editText5);
         TextView podL2 = findViewById(R.id.editText8);
-        if   ((podL2.getText().length() != 0))
-             stanL95 = Integer.parseInt(podL2.getText().toString());
+        if ((podL2.getText().length() != 0))
+            stanL95 = Integer.parseInt(podL2.getText().toString());
         else stanL95 = 1;
-        if  ((stanL95 >= bazaL95) && (stanL95 < bazaL95 + 2000)) {
-             Suma L95 = new Prad(n95, stanL95, bazaL95, ab95, do95);
-             textL2.setText(L95.toString());
+        if ((stanL95 >= bazaL95) && (stanL95 < bazaL95 + 2000)) {
+            Suma L95 = new Prad(n95, stanL95, bazaL95, ab95, do95);
+            textL2.setText(L95.toString());
         } else textL2.setText("nieprawidłowe dane");
+    }
+
+    public void druga(View view) {
+        Intent drugaIntent = new Intent(getApplicationContext(), DrugaActivity.class);
+        drugaIntent.putExtra("aupa", "stan licznika wody");
+        drugaIntent.putExtra("bupa", "stan liczników energii");
+        drugaIntent.putExtra("cupa", "stan licznika 95");
+
+        drugaIntent.putExtra("xupa", "tel");
+        drugaIntent.putExtra("yupa", "net");
+        drugaIntent.putExtra("zupa", "połącz");
+
+
+
+
+        startActivity(drugaIntent);
+
+
+
+
+    }
+
+    public void mapa(View view) {
+
+        Intent mapka = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:49.98627, 018.42030?z=16"));
+        if (mapka.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapka);
+        }
+
+    }
+
+    public void net(View view) {
+
+
+
     }
 }
